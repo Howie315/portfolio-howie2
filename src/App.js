fdf664585ae2011c6a230617416e83c5f5d6cb78
Howie315 <nguyen.howie2010@gmail.com>
2023-12-11 22:19:29 -0800

    first panel done

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import "./App.css";
 import { BrowserRouter as Router } from "react-router-dom";
 
 import Navigation from "./features/Navigation/presentation/Navigation";
-
+import FirstPanel from "./features/FirstPanel/presentation/FirstPanel";
 function App() {
   return (
     <Router>
       <div className="App">
         <Navigation />
+        <FirstPanel />
       </div>
     </Router>
   );
---
+++ src/features/FirstPanel/presentation/FirstPanel.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './FirstPanel.scss';
+import Lottie from 'react-lottie-player';
+import Space from "../../../Animation - 1702361057646.json";
+
+const FirstPanel = () => {
+  return (
+    <div className="first-panel">
+          <Lottie 
+        loop
+        animationData={Space}
+        play
+        className="lottie-animation"
+      />
+      <h1 className="greeting">Hi There, it's Howie :)</h1>
+      <h2 className="service-message">At Your Service</h2>
+    </div>
+  );
+};
+
+export default FirstPanel;
