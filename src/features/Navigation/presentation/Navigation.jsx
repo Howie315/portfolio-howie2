23405380c0fd052cd742bbe49a1bbc8c92cb5f95
Howie315 <nguyen.howie2010@gmail.com>
2024-01-22 23:13:57 -0800

    added mobile better now

--- src/features/Navigation/presentation/Navigation.jsx
+++ src/features/Navigation/presentation/Navigation.jsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { HashLink as Link } from "react-router-hash-link";
 import NameLogo from "../../../imgs/angle-right.svg"; // Replace with the path to your photo
 import "./Navigation.scss";
 import gsap from "gsap";
 import ScrollToPlugin from "gsap/ScrollToPlugin";
+import navIcon from "../../../imgs/icons8-hamburger-menu.svg";
 gsap.registerPlugin(ScrollToPlugin);
 
 const Navigation = () => {
+  const [isNavVisible, setIsNavVisible] = useState(false);
+
+  const toggleNav = () => {
+    setIsNavVisible(!isNavVisible);
+  };
   const handleClick = (e, target) => {
     e.preventDefault(); // Prevent default anchor behavior
     gsap.to(window, { duration: 1, scrollTo: target }); // Smooth scroll to target
@@ -18,38 +24,36 @@ const Navigation = () => {
         <img src={NameLogo} alt="Your Name" className="logo" />
       </div>
 
-      <nav className="nav-links">
-        <ul>
-          <li className="nav-item">
-            {" "}
-            <a href="#about" onClick={(e) => handleClick(e, "#about")}>
-              about
-            </a>
-          </li>
-          <li className="nav-item">
-            {" "}
-            <a
-              href="#experience"
-              onClick={(e) => handleClick(e, "#experience")}
-            >
-              experience
-            </a>
-          </li>
-          <li className="nav-item">
-            {" "}
-            <a href="#projects" onClick={(e) => handleClick(e, "#projects")}>
-              projects
-            </a>
-          </li>
-          <li className="contact-button">
-            <a href="mailto:nguyen.howie2010@gmail.com">Let's Contact</a>
-          </li>
-        </ul>
-      </nav>
-
-      {/* <div id="about"></div>
-      <div id="experience"></div>
-      <div id="projects"></div> */}
+      {isNavVisible && (
+        <nav className="nav-links">
+          <ul>
+            <li className="nav-item">
+              {" "}
+              <a href="#about" onClick={(e) => handleClick(e, "#about")}>
+                about
+              </a>
+            </li>
+            <li className="nav-item">
+              {" "}
+              <a
+                href="#experience"
+                onClick={(e) => handleClick(e, "#experience")}
+              >
+                experience
+              </a>
+            </li>
+            <li className="nav-item">
+              {" "}
+              <a href="#projects" onClick={(e) => handleClick(e, "#projects")}>
+                projects
+              </a>
+            </li>
+            <li className="contact-button">
+              <a href="mailto:nguyen.howie2010@gmail.com">Let's Contact</a>
+            </li>
+          </ul>
+        </nav>
+      )}
     </div>
   );
 };
